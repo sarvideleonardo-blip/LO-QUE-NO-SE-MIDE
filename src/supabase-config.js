95858cce6bdc39350242f4035b906b38cde51d20
sarvideleonardo-blip <sarvideleonardo@gmail.com>
2026-09-11 11:56:17 -0600

    Connect Zombies Lunares to Supabase runs

---
+++ src/supabase-config.js
@@ -0,0 +1,5 @@
+window.ZOMBIES_LUNARES_SUPABASE = {
+  url: 'https://lseglmysqwdbfylxgoxx.supabase.co',
+  publishableKey: 'sb_publishable_cFmnuO97yZZrJvd_k_rmmQ_BqLfkWQ6',
+  runsTable: 'zombies_lunares_runs',
+};
